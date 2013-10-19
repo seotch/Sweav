017c0ba17f12dd87f9f7aa432831e06b25543a3c
seotch <seotch@gmail.com>
2013-10-20 03:12:19 +0900

    First Commit

---
+++ src/characterBuilder/CharacterBuilder.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package characterBuilder;
+
+/**
+ *
+ * @author 瀬尾浩史
+ */
+public class CharacterBuilder {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+       System.out.println("Hello World!");
+      System.out.println("Hello World!");
+      System.out.println("Hello World!");
+        
+       
+   }
+    
+}
