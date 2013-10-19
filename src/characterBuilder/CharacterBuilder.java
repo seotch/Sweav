01887606aa4b84ee84773dfb7ade1837d8f93dd9
Hiroshi Seo <seotch@gmail.com>
2013-10-20 03:48:13 +0900

    U310からコミット

--- src/characterBuilder/CharacterBuilder.java
+++ src/characterBuilder/CharacterBuilder.java
@@ -15,7 +15,7 @@ public class CharacterBuilder {
      */
     public static void main(String[] args){
         System.out.println("Hello World!");
-       System.out.println("Hello World!");
+       System.out.println("Hello Akihabara!");
 
         
        
