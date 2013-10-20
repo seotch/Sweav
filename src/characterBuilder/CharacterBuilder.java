eaa095e5d0043b560bd4299d2b40e9e9c48776cd
seotch <seotch@gmail.com>
2013-10-20 16:37:03 +0900

    Branch Test3

--- src/characterBuilder/CharacterBuilder.java
+++ src/characterBuilder/CharacterBuilder.java
@@ -14,6 +14,8 @@ public class CharacterBuilder {
      * @param args
      */
     public static void main(String[] args){
+        
+       System.out.println("Test Branch3!");
         System.out.println("Hello World!");
         System.out.println("Hello Akihabara!");
 	System.out.println("Hello Meguro!");
