af1ed7467a4ff91c8fae418f977981013f9cc615
seotch <seotch@gmail.com>
2013-10-20 21:54:26 +0900

    Test

---
+++ src/character/Character.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package character;
+
+/**
+ *
+ * @author 瀬尾浩史
+ */
+public class Character {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args){
+        
+        System.out.println("Test Branch3!");
+        System.out.println("Hello World!");
+        System.out.println("Hello Akihabara!");
+	System.out.println("Hello Meguro!");
+	System.out.println("Hello Yoyogi!");
+	System.out.println("Hello Yokohama!");
+ 	System.out.println("Test Branch!");
+ 	System.out.println("Test Branch2!");
+   }
+    
+    
+}
